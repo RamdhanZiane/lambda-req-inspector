9502f4a9e74e2bc38528315ddcfd14be88ca7a07
Ramdhan <issakalem@gmail.com>
2023-05-22 18:03:03 +0100

    allowing all hosts

--- lambdaInsector/settings.py
+++ lambdaInsector/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8+j#om93d5eo&qv@=nd(5t4u_jo9-4%@*499b(=a8pu18omer5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
